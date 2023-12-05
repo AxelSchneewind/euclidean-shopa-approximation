b83b78d414a758d5ff709c123e6f683cd462f420
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2023-12-05 16:36:23 +0100

    adjust scripts

--- scripts/compute_stats.sh
+++ scripts/compute_stats.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 
-for epsilon in 2.56 1.28 0.64 0.32 0.16 0.08 0.04 0.02 0.01 0.005
+for epsilon in 4.0 2.0 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625
 do
 	# /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/aegaeis/pruned.graph $epsilon >> graph_sizes.txt
-  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/route_interactive_steiner /home/axel/Studium/Semester_7/graphs/aegaeis/pruned.graph /home/axel/Studium/Semester_7/graphs/aegaeis/ $epsilon 2210302 235261 >> graph_sizes.txt
+  echo computing for ε = $epsilon ...
+  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/route_interactive_steiner /home/axel/Studium/Semester_7/graphs/aegaeis/pruned.graph /home/axel/Studium/Semester_7/graphs/aegaeis/ $epsilon 2210303 235261 >> statistics.csv
+
 done
 
--- scripts/show_graph_sizes.py
+++ scripts/show_graph_sizes.py
@@ -2,16 +2,30 @@
 
 import pandas as pd
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+import math
 
 
 # read graph statistics
-data = pd.read_csv('statistics.csv', sep=',', comment='#')
+data = pd.read_csv('graph_sizes.csv', sep=',', comment='#')
 data.sort_values('epsilon', inplace=True)
+
+# 
+node_count = data['stored node count'][0]
+edge_count = data['stored edge count'][0]
 
 # columns
 xValues = data['epsilon']
 nValues = data['node count']
 eValues = data['edge count']
+
+# reference
+num_values = len(xValues)
+coefficient = nValues[num_values - 1] * pow(xValues[num_values-1], 1)
+# rValues = [ coefficient / pow(x, 1) for x in xValues ]
+#other_coefficient = (nValues[num_values-1] * xValues[num_values-1] * -1 / math.log(xValues[num_values -1], 2))
+other_coefficient = edge_count
+otherRValues = [ (coefficient / x * -1 * math.log(x, 2)) + node_count if x < 1.0 else node_count for x in xValues ] 
 
 # 
 (fig, ax) = plt.subplots(1,2)
@@ -34,11 +48,13 @@ edges.hlines(coastlines_edge_count, xmin, xmax, label='coastlines base edge coun
 
 nodes.plot(xValues, nValues, label='number of nodes')
 edges.plot(xValues, eValues, label='number of edges (undirected)')
+# nodes.plot(xValues, rValues, label='in Theta(1/(ε^2.5))')
+nodes.plot(xValues, otherRValues, label='in Theta(1/ε * log(1/ε))')
 
 nodes.set_yscale('log')
 edges.set_yscale('log')
-nodes.set_xscale('log')
-edges.set_xscale('log')
+nodes.set_xscale(mpl.scale.LogScale(nodes, base=2))
+edges.set_xscale(mpl.scale.LogScale(edges, base=2))
 
 
 nodes.set_xlabel('ε')
