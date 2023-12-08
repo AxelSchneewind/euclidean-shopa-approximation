1d02b854a888eb3ab34b3c1bdebe2f727147ffc1
Axel Schneewind <axel.schneewind@protonmail.com>
2023-12-08 17:49:36 +0100

    add command line parsing to route executable

--- src/route.cpp
+++ src/route.cpp
@@ -1,6 +1,6 @@
 #include "routing.h"
 
-#include "cli/cmdline_route_interactive.h"
+#include "cli/cmdline_route.h"
 
 #include <format>
 #include <fstream>
@@ -10,60 +10,61 @@
 
 int
 main(int argc, char const *argv[]) {
+    gengetopt_args_info arguments;
+
+    int status = cmdline_parser(argc, (char **) argv, &arguments);
+    if (status != 0) {
+        return 1;
+    }
+
+    if (arguments.help_given != 0) {
+        std::cout << arguments.help_help << std::endl;
+        return 0;
+    }
+
+
     std::string graph_file;
     std::string output_directory;
     float epsilon = 0.5;
 
-    if (argc > 3) {
-        graph_file = argv[1];
-        output_directory = argv[2];
-        epsilon = std::stof(argv[3]);
-    } else {
-        std::cout << "graph file: ";
-        std::cin >> graph_file;
-
-        std::cout << "output directory: ";
-        std::cin >> output_directory;
-
-        std::cout << "epsilon: ";
-        std::cin >> epsilon;
-    }
-
+    graph_file = arguments.graph_file_arg;
+    output_directory = arguments.output_directory_arg;
+    epsilon = arguments.epsilon_arg;
 
     // read graph
     Client client;
     if (graph_file.ends_with(".graph"))
-        client.read_graph_file(graph_file, epsilon, true);
+        client.read_graph_file(graph_file, epsilon, (bool) arguments.csv_format_flag);
     else
-        client.read_graph_file(graph_file, true);
+        client.read_graph_file(graph_file, (bool) arguments.csv_format_flag);
 
     client.write_graph_stats(std::cout);
 
     bool done = false;
-    bool from_stdin = (argc < 6);
+    bool from_stdin = (arguments.query_given / 2 < 1) || arguments.stdin_flag;
+    int query_index = 0;
 
-    int source_dest_index = 4;
     while (!done) {
         // get query
         int src_node(0);
         int dest_node(0);
 
-        if (!from_stdin) {
-            src_node = std::stoi(argv[source_dest_index++]);
-            dest_node = std::stoi(argv[source_dest_index++]);
-            if (argc <= source_dest_index + 1)
-                done = true;
-        } else {
+        if (query_index + 1 < arguments.query_given) {
+            src_node = arguments.query_arg[query_index++];
+            dest_node = arguments.query_arg[query_index++];
+        } else if (from_stdin) {
             std::cout << "src node: " << std::flush;
             std::cin >> src_node;
             std::cout << "dest node: " << std::flush;
             std::cin >> dest_node;
             std::cout << std::endl;
+        } else {
+            break;
         }
 
         // setup writer for graphs to show
-        std::string target_directory = std::format("{}/{}_{}_{}", output_directory, src_node, dest_node,
-                                                   (int) (epsilon * 100));
+        std::string eps_string = epsilon == 0 ? "exact" : std::format("{:_>5d}", (int) epsilon * 1000);
+        std::string target_directory = std::format("{}/{}_{}_{}", output_directory, src_node, dest_node, eps_string);
         std::filesystem::create_directory(target_directory);
         std::string beeline_file = std::format("{}/beeline.gl", target_directory);
         std::string route_file = std::format("{}/route.gl", target_directory);
