5938b2ab5b8cfc65e8b201cac4674cdc3d84233a
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2023-12-28 12:43:51 +0100

    change script to compute graph sizes

--- scripts/compute_stats.sh
+++ scripts/compute_stats.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+/home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-ref-projected.graph 8.0 --header | tee -a graph_sizes.csv
 
-for epsilon in 4.0 2.0 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625
+for epsilon in 4.0 2.0 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125
 do
   echo computing for ε = $epsilon ...
-  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/route_interactive_steiner /home/axel/Studium/Semester_7/graphs/aegaeis/pruned.graph /home/axel/Studium/Semester_7/graphs/aegaeis/ $epsilon 2210303 235261 >> statistics.csv
-
+  # compute graph sizes
+  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-ref-projected.graph $epsilon | tee -a graph_sizes.csv
 done
 
+
+/home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-unref-projected.graph 8.0 --header | tee -a graph_sizes.csv
+for epsilon in 4.0 2.0 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125
+do
+  echo computing for ε = $epsilon ...
+  # compute graph sizes
+  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-unref-projected.graph $epsilon | tee -a graph_sizes.csv
+done
