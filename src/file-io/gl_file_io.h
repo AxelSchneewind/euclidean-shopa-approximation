573ee8c75d01f487e78db016e58ed38840b93618
Axel Schneewind <axel.schneewind@protonmail.com>
2024-01-01 18:48:09 +0100

    set default linewidth to 1

--- src/file-io/gl_file_io.h
+++ src/file-io/gl_file_io.h
@@ -8,7 +8,7 @@
 class gl_file_io {
 public:
     template<Topology Graph, typename format = stream_encoders::encode_text>
-    static std::ostream &write(std::ostream &output, const Graph &graph, int line_width = 2, int color = 2);
+    static std::ostream &write(std::ostream &output, const Graph &graph, int line_width = 1, int color = 2);
 
     template<typename Graph, typename format = stream_encoders::encode_text>
     static Graph read(std::istream &input) { return fmi_file_io::read<Graph, format>(input, input, input); };
