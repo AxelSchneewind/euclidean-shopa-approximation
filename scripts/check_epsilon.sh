b32b6e3cb9c01c8e4096b1e94ecb33891b0b915f
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-01-05 11:03:36 +0100

    use installed route executable

--- scripts/check_epsilon.sh
+++ scripts/check_epsilon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ROUTER=/home/axel/Studium/Semester_7/routing/cmake-build-release/route
+ROUTER=route
 
 VISIBILITY_GRAPH=/home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis-visibility.fmi
-TRIANGULATION_GRAPH=/home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-ref.graph
+TRIANGULATION_GRAPH=/home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-ref-projected.graph
 TRIANGULATION_UNREF_GRAPH=/home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-unref.graph
 
 VISIBILITY_RESULTS=/home/axel/Studium/Semester_7/graphs/visibility_aegaeis/results_exact
@@ -24,27 +24,27 @@ for i in "${NODES[@]}" ; do
 done
 QUERY=${QUERY::-1}
 
-QUERY_UNREF=""
-for i in "${NODES_UNREF[@]}" ; do
-  for j in "${NODES_UNREF[@]}" ; do
-    QUERY+="$i,$j,"
-  done
-done
-QUERY_UNREF=${QUERY_UNREF::-1}
+# QUERY_UNREF=""
+# for i in "${NODES_UNREF[@]}" ; do
+#   for j in "${NODES_UNREF[@]}" ; do
+#     QUERY+="$i,$j,"
+#   done
+# done
+# QUERY_UNREF=${QUERY_UNREF::-1}
 
-echo "##################### computing exact values: #####################"
+# echo "##################### computing exact values: #####################"
+# echo "########################## exact values: ##########################" >> out.log
+# echo "$ROUTER --graph-file $VISIBILITY_GRAPH --output-directory $VISIBILITY_RESULTS --epsilon 0.0 --query $QUERY" -c
+# $ROUTER --graph-file $VISIBILITY_GRAPH --output-directory $VISIBILITY_RESULTS --epsilon 0.0 --query $QUERY -c | tee --append out.log
 
-echo "########################## exact values: ##########################" >> out.log
-echo "$ROUTER --graph-file $VISIBILITY_GRAPH --output-directory $VISIBILITY_RESULTS --epsilon 0.0 --query $QUERY" -c
-$ROUTER --graph-file $VISIBILITY_GRAPH --output-directory $VISIBILITY_RESULTS --epsilon 0.0 --query $QUERY -c | tee --append out.log
 
-
-echo "########## computing approximate values in unref graph:  ##########"
-echo "############### approximate values in unref graph:  ###############" >> out.log
-echo "$ROUTER --graph-file $TRIANGULATION_UNREF_GRAPH --output-directory $TRIANGULATION_UNREF_RESULTS --epsilon 0.2 --query $QUERY_UNREF" -c
-$ROUTER --graph-file $TRIANGULATION_UNREF_GRAPH --output-directory $TRIANGULATION_UNREF_RESULTS --epsilon 0.2 --query $QUERY_UNREF -c | tee --append out.log
+# echo "########## computing approximate values in unref graph:  ##########"
+# echo "############### approximate values in unref graph:  ###############" >> out.log
+# echo "$ROUTER --graph-file $TRIANGULATION_UNREF_GRAPH --output-directory $TRIANGULATION_UNREF_RESULTS --epsilon 0.2 --query $QUERY_UNREF" -c
+# $ROUTER --graph-file $TRIANGULATION_UNREF_GRAPH --output-directory $TRIANGULATION_UNREF_RESULTS --epsilon 0.2 --query $QUERY_UNREF -c | tee --append out.log
 
 echo "################## computing approximate values: ##################"
 echo "####################### approximate values: #######################" >> out.log
 echo "$ROUTER --graph-file $TRIANGULATION_GRAPH --output-directory $TRIANGULATION_RESULTS --epsilon 0.2 --query $QUERY" -c
-$ROUTER --graph-file $TRIANGULATION_GRAPH --output-directory $TRIANGULATION_RESULTS --epsilon 0.2 --query $QUERY -c | tee --append out.log
+$ROUTER --graph-file $TRIANGULATION_GRAPH --output-directory $TRIANGULATION_RESULTS --epsilon 0.2 --query $QUERY -p wgs84 | tee --append out.log
+render_graph $(echo ${"$(echo ../../graphs/visibility_aegaeis/results_steiner/*/path.gl)"//\ /\ -gf\ })
