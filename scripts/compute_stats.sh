2cc2438656aea0abcaec9a4f93b19e162c2eb137
Axel Schneewind <axel.schneewind@protonmail.com>
2024-01-09 15:59:56 +0100

    simplify graph stats script

--- scripts/compute_stats.sh
+++ scripts/compute_stats.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+#
 
-/home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-ref-projected.graph 8.0 --header | tee -a graph_sizes.csv
+GRAPH=/opt/routing/graphs/visibility_aegaeis/aegaeis10-ref-projected.graph
+GRAPH_PATA=/opt/routing/graphs/visibility_aegaeis/aegaeis10-unref-projected.graph
+GRAPH_MEDI=/opt/routing/graphs/visibility_aegaeis/aegaeis10-unref-projected.graph
 
-# for epsilon in 4.0 2.0 1.0 '0.5' '0.25' '0.125' '0.0625' '0.03125' '0.015625' '0.0078125' '0.00390625' '0.001953125'
+subdivision_stats graph $GRAPH 8.0 --header | tee -a graph_sizes.csv
+
+compute () {
 for epsilon in 4.0 2.0 1.0 '1/2' '1/4' '1/8' '1/16' '1/32' '1/64' '1/128' '1/256'
 do
   echo computing for ε = $epsilon ...
   # compute graph sizes
-  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-ref-projected.graph $epsilon | tee -a graph_sizes.csv
+  subdivision_stats graph $1 $epsilon | tee -a graph_sizes.csv
 done
+}
 
 
-/home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-unref-projected.graph 8.0 --header | tee -a graph_sizes.csv
-for epsilon in 4.0 2.0 1.0 '1/2' '1/4' '1/8' '1/16' '1/32' '1/64' '1/128' '1/256'
-do
-  echo computing for ε = $epsilon ...
-  # compute graph sizes
-  /home/axel/Dokumente/Studium/Semester_7/routing/cmake-build-release/subdivision_stats graph /home/axel/Studium/Semester_7/graphs/visibility_aegaeis/aegaeis10-unref-projected.graph $epsilon | tee -a graph_sizes.csv
-done
+compute $GRAPH
+compute $GRAPH_PATA
+compute $GRAPH_MEDI
