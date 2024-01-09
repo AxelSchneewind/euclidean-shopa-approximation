bcb88a79efcb4f46923b316821f600c3c824518b
Axel Schneewind <axel.schneewind@protonmail.com>
2024-01-09 11:33:08 +0100

    preliminary fix for router abort criterion

--- src/routing/router_impl.h
+++ src/routing/router_impl.h
@@ -1,104 +1,100 @@
 #pragma once
 
 #include <cassert>
-#include <exception>
 
 #include "router.h"
 
-#include "../graph/graph_impl.h"
-#include "dijkstra_impl.h"
-#include <deque>
-#include <map>
-#include <ostream>
-
-#include "dijkstra_queues.h"
-
-
 template<typename Graph, typename Dijkstra>
-Graph::distance_type
+typename router<Graph, Dijkstra>::distance_type
 router<Graph, Dijkstra>::min_route_distance(Dijkstra::node_cost_pair_type node) const {
     return node.min_distance();
 }
 
 template<typename Graph, typename Dijkstra>
-router<Graph, Dijkstra>::router(Graph const &graph)
-        : _M_graph(graph),
-          _M_forward_search(_M_graph),
-          _M_start_node(none_value<typename Graph::node_id_type>),
-          _M_target_node(none_value<typename Graph::node_id_type>),
-          _M_mid_node(none_value<typename Graph::node_id_type>) {}
+router<Graph, Dijkstra>::router(Graph const&graph)
+    : _graph(graph),
+      _forward_search(_graph),
+      _start_node(none_value<typename Graph::node_id_type>),
+      _target_node(none_value<typename Graph::node_id_type>),
+      _mid_node(none_value<typename Graph::node_id_type>) {
+}
 
 template<typename Graph, typename Dijkstra>
-router<Graph, Dijkstra>::router(router &&routing) noexcept
-        : _M_graph(routing._M_graph),
-          _M_forward_search(std::move(routing._M_forward_search)),
-          _M_start_node(routing._M_start_node), _M_target_node(routing._M_target_node),
-          _M_mid_node(routing._M_mid_node) {
-    routing._M_mid_node = none_value<typename Graph::node_id_type>;
+router<Graph, Dijkstra>::router(router&&routing) noexcept
+    : _graph(routing._graph),
+      _forward_search(_graph, std::move(routing._forward_search)),
+      _start_node(routing._start_node), _target_node(routing._target_node),
+      _mid_node(routing._mid_node) {
+    routing._mid_node = none_value<typename Graph::node_id_type>;
 }
 
 template<typename Graph, typename Dijkstra>
 void
 router<Graph, Dijkstra>::step_forward() {
-    assert (!_M_forward_search.queue_empty());
-
-    _forward_current = _M_forward_search.current();
     assert(!is_none(_forward_current.node));
+    // TODO check why this is never true
+    if (_forward_current.node == _target_node)
+        _mid_node = _target_node;
 
-    _M_forward_search.step();
+    assert(!_forward_search.queue_empty());
 
-    if (_forward_current.node == _M_target_node)
-        _M_mid_node = _M_target_node;
+    _forward_search.step();
+    _forward_current = _forward_search.current();
 }
 
 template<typename Graph, typename Dijkstra>
 void
 router<Graph, Dijkstra>::compute_route() {
-    bool done = _M_forward_search.queue_empty() || min_route_distance(_M_forward_search.current()) > distance();
+    bool done = _forward_search.queue_empty();
     std::size_t step_count = 0;
     while (!done) {
+        done = _forward_search.queue_empty();
+        done |= !is_none(_forward_search.get_label(_target_node).predecessor) && !is_infinity(_forward_search.get_label(_target_node).distance);
+
         step_forward();
 
-        done = _M_forward_search.queue_empty() || min_route_distance(_M_forward_search.current()) > distance();
+        done |= is_none(_forward_current);
         step_count++;
     }
+
+    _mid_node = _target_node;
 }
 
 template<typename Graph, typename Dijkstra>
-Graph::distance_type
-router<Graph, Dijkstra>::distance(const Graph::node_id_type &node) const {
-    if (is_none(node) || !_M_forward_search.reached(node)) {
+typename router<Graph, Dijkstra>::distance_type
+router<Graph, Dijkstra>::distance(const Graph::node_id_type&node) const {
+    if (is_none(node) || !_forward_search.reached(node)) {
         return infinity<typename Graph::distance_type>;
     }
-    return _M_forward_search.get_label(node).distance;
+    return _forward_search.get_label(node).distance;
 }
 
 template<typename Graph, typename Dijkstra>
-Graph::distance_type
+typename router<Graph, Dijkstra>::distance_type
 router<Graph, Dijkstra>::distance() const {
-    if (is_none(_M_mid_node)) {
+    if (is_none(_mid_node)) {
         return infinity<typename Graph::distance_type>;
     }
-    return _M_forward_search.get_label(_M_mid_node).distance;
+    return _forward_search.get_label(_mid_node).distance;
 }
 
 template<typename Graph, typename Dijkstra>
-Graph::path_type
+typename Graph::path_type
 router<Graph, Dijkstra>::route() const {
-    if (is_none(_M_mid_node))
+    if (is_none(_mid_node))
         return path<Graph>();
 
-    auto path_fwd = _M_forward_search.path(_M_mid_node);
+    auto path_fwd = _forward_search.path(_mid_node);
     return path_fwd;
 };
 
 template<typename Graph, typename Dijkstra>
-Graph::subgraph_type
+typename Graph::subgraph_type
 router<Graph, Dijkstra>::shortest_path_tree() const {
-    auto tree_fwd = _M_forward_search.shortest_path_tree();
+    auto tree_fwd = _forward_search.shortest_path_tree();
 
     // filter_nodes(tree_fwd, [&](auto node) -> bool {
-    //     return _M_forward_search.get_label(node).distance + ::distance(_M_graph.node(node).coordinates, _M_graph.node(_M_target_node).coordinates) <= distance();
+    //     return _forward_search.get_label(node).distance + ::distance(_graph.node(node).coordinates, _graph.node(_target_node).coordinates) <= distance();
     // });
 
     return tree_fwd;
@@ -107,17 +103,17 @@ router<Graph, Dijkstra>::shortest_path_tree() const {
 template<typename Graph, typename Dijkstra>
 bool
 router<Graph, Dijkstra>::route_found() const {
-    return !is_none(_M_mid_node) && _M_forward_search.reached(_M_mid_node);
+    return !is_none(_mid_node) && _forward_search.reached(_mid_node);
 }
 
 template<typename Graph, typename Dijkstra>
 void
-router<Graph, Dijkstra>::init(Graph::node_id_type start_node, Graph::node_id_type target_node) {
-    _M_start_node = start_node;
-    _M_target_node = target_node;
-    _M_mid_node = none_value<typename Graph::node_id_type>;
+router<Graph, Dijkstra>::init(node_id_type start_node, node_id_type target_node) {
+    _start_node = start_node;
+    _target_node = target_node;
+    _mid_node = none_value<node_id_type>;
 
-    _M_forward_search.init(_M_start_node, _M_target_node);
+    _forward_search.init(_start_node, _target_node);
 
-    _forward_current = _M_forward_search.current();
+    _forward_current = _forward_search.current();
 }
