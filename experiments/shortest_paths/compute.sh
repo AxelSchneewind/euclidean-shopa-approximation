ff5aeae2c3ee83ecaa5c9b83baadb50acbf4b42a
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-02-09 17:43:32 +0100

    fix compute script

--- experiments/shortest_paths/compute.sh
+++ experiments/shortest_paths/compute.sh
@@ -2,7 +2,5 @@
 
 SCRIPTS=($(echo compute_*.sh))
 for script in "${SCRIPTS[@]}"; do
-	echo "$script"
-	echo ""
-	# $(sh -c "./$script" > out_$script.txt 2>&1 &)
+	(sh -c "./$script" > out_$script.txt 2>&1 &)
 done
