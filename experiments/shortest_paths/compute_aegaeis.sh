e58fc3a3c9c90a9cc27c2e24ae6f5bbbbaf60978
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-03-11 17:49:32 +0100

    only run benchmarks if results nonexistent

--- experiments/shortest_paths/compute_aegaeis.sh
+++ experiments/shortest_paths/compute_aegaeis.sh
@@ -23,32 +23,47 @@ QUERY_FILE=results/aegaeis/queries.txt
 mkdir -p "$OUTPUT_DIR"
 
 # make queries
-make_queries "$UNREF_TRIANGULATION_GRAPH" "$QUERY_FILE" "$NUM_QUERIES"
+if [ ! -f "$QUERY_FILE" ]; then
+	make_queries "$UNREF_TRIANGULATION_GRAPH" "$QUERY_FILE" "$NUM_QUERIES"
+fi
+
 
 # refined graph
-compute_single "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/raw" "$QUERY_FILE" inf
-EPSILONS=("1.0" "0.5" "0.2" "0.1" "0.05" "0.02")
-for eps in "${EPSILONS[@]}"; do
-    compute_single "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/approximate" "$QUERY_FILE" "$eps"
-done
+if [ ! -d "$OUTPUT_DIR/raw" ]; then
+	compute_single "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/raw" "$QUERY_FILE" inf
+fi
+process_results "$OUTPUT_DIR/raw" "$OUTPUT_DIR/results-raw.csv"
 
-# unrefined graph
-compute_single "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/raw-unref" "$QUERY_FILE" inf
-# EPSILONS=("1.0" "0.5" "0.2" "0.1")
-EPSILONS=("1.0" "0.5")
-for eps in "${EPSILONS[@]}"; do
-    compute_single "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/approximate-unref" "$QUERY_FILE" "$eps"
-done
+# refined graph with steiner points
+if [ ! -d "$OUTPUT_DIR/approximate" ]; then
+	EPSILONS=("1.0" "0.5" "0.2" "0.1" "0.05" "0.02")
+	for eps in "${EPSILONS[@]}"; do
+	    compute_single "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/approximate" "$QUERY_FILE" "$eps"
+	done
+fi
+process_results "$OUTPUT_DIR/approximate" "$OUTPUT_DIR/results-approximate.csv"
+
+# unrefined graph raw
+if [ ! -d "$OUTPUT_DIR/raw-unref" ]; then
+	compute_single "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/raw-unref" "$QUERY_FILE" inf
+fi
+process_results "$OUTPUT_DIR/raw-unref" "$OUTPUT_DIR/results-raw-unref.csv"
+
+# unrefined graph with steiner points
+if [ ! -d "$OUTPUT_DIR/approximate-unref" ]; then
+	# EPSILONS=("1.0" "0.5" "0.2" "0.1")
+	EPSILONS=("1.0" "0.5")
+	for eps in "${EPSILONS[@]}"; do
+	    compute_single "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/approximate-unref" "$QUERY_FILE" "$eps"
+	done
+fi
+process_results "$OUTPUT_DIR/approximate-unref" "$OUTPUT_DIR/results-approximate-unref.csv"
 
 # exact solutions
-# compute_single "$VISIBILITY_GRAPH" "$OUTPUT_DIR/exact" "$QUERY_FILE" 0.0
-
-# process results for each case
-process_results "$OUTPUT_DIR/raw" "$OUTPUT_DIR/results-raw.csv"
-process_results "$OUTPUT_DIR/raw-unref" "$OUTPUT_DIR/results-raw-unref.csv"
-process_results "$OUTPUT_DIR/approximate" "$OUTPUT_DIR/results-approximate.csv"
-process_results "$OUTPUT_DIR/approximate-unref" "$OUTPUT_DIR/results-approximate-unref.csv"
-# process_results "$OUTPUT_DIR/exact" "$OUTPUT_DIR/results-exact.csv"
+if [ ! -d "$OUTPUT_DIR/exact" ]; then
+	compute_single "$VISIBILITY_GRAPH" "$OUTPUT_DIR/exact" "$QUERY_FILE" 0.0
+fi
+process_results "$OUTPUT_DIR/exact" "$OUTPUT_DIR/results-exact.csv"
 
 # aggregate results into one file
 process_all_results "$OUTPUT_DIR" "$OUTPUT_DIR"/results.csv
--- experiments/shortest_paths/utils.sh
+++ experiments/shortest_paths/utils.sh
@@ -47,62 +47,7 @@ make_queries(){
     echo "${NODES[@]}"
 }
 
-#
-compute() {
-    local GRAPH_FILE=$1
-    local GRAPH_FILE_VISIBILITY=$2
-    
-    # twice the number of queries to run per graph
-    local NUM_QUERIES=$3
-    
-    #
-    local GRAPH_NAME=$(basename "$GRAPH_FILE")
-    local GRAPH_NAME=${GRAPH_NAME%.graph}
-    
-    # make empty output directories
-    local OUTPUT_DIR="results/${GRAPH_NAME}/approximate/"
-    local OUTPUT_DIR_VIS="results/${GRAPH_NAME}/visibility/"
-    local OUTPUT_DIR_RAW="results/${GRAPH_NAME}/raw/"
-    mkdir -p "$OUTPUT_DIR"
-    mkdir -p "$OUTPUT_DIR_VIS"
-    mkdir -p "$OUTPUT_DIR_RAW"
-    rm -rf "${OUTPUT_DIR:?}"/*
-    rm -rf "${OUTPUT_DIR_VIS:?}"/*
-    rm -rf "${OUTPUT_DIR_RAW:?}"/*
-    
-    # select given number of nodes randomly from boundary nodes in triangulation
-    local NUM_NODES=$((2 * NUM_QUERIES))
-    echo "selecting $NUM_NODES nodes from graph $GRAPH_NAME..."
-    local NODES=$($FIND_NODES -g "$GRAPH_FILE" -b -r $NUM_NODES)
-    local NODES=$(echo "$NODES" | sed -z -E "s/\n/,/g")
-    local NODES=${NODES::-1}
-    echo "$NODES" > "results/${GRAPH_NAME}/nodes.txt"
-    echo "${NODES[@]}"
-    
-    # make queries
-    local QUERY=""
-    for i in "${NODES[@]}" ; do
-      local QUERY+="$i,"
-    done
-    local QUERY=${QUERY::-1}
-    
-    # run shortest path computations
-    echo " ################# computing approximate values without subdividing triangulation:  ################# "
-    echo "$ROUTER --graph-file $GRAPH_FILE --output-directory $OUTPUT_DIR_RAW --epsilon inf --query $QUERY -p wgs84 -a $ASTAR -t$TREE_SIZE"
-    $ROUTER --epsilon inf -p wgs84 -a "$ASTAR" -t$TREE_SIZE -l --graph-file "$GRAPH_FILE" --output-directory "$OUTPUT_DIR_RAW" --query "$QUERY"
-    
-    local EPSILONS=("1.0" "0.5" "0.2" "0.1" "0.05")
-    # "0.02" "0.01")
-    for eps in "${EPSILONS[@]}"; do
-    echo " ########################## computing approximate values for epsilon=$eps: ########################## "
-    echo "$ROUTER --graph-file $GRAPH_FILE --output-directory $OUTPUT_DIR --epsilon $eps --query $QUERY -p wgs84 -a $ASTAR -t$TREE_SIZE"
-    $ROUTER --epsilon "$eps"  -p wgs84 -a "$ASTAR" -t$TREE_SIZE -l --graph-file "$GRAPH_FILE" --output-directory "$OUTPUT_DIR" --query "$QUERY"
-    done
-    
-    echo " ##################################### computing exact values:  ##################################### "
-    echo "$ROUTER --graph-file $GRAPH_FILE_VISIBILITY --output-directory $OUTPUT_DIR_VIS -e 0.0 --query $QUERY -p wgs84 -a $ASTAR -t$TREE_SIZE"
-    $ROUTER -e 0.0 -p wgs84 -a "$ASTAR" -t$TREE_SIZE -l --graph-file "$GRAPH_FILE_VISIBILITY" --output-directory "$OUTPUT_DIR_VIS" --query "$QUERY"
-}
+
 
 process_results() {
     if [[ -z "$2" ]]; then
@@ -127,7 +72,7 @@ process_results() {
     sed -e '1,1s/$/,benchmark/' -i "$CSV_RESULTS"
     sed -e '2,$s/,$/,,OUTPUT_DIR/' -i "$CSV_RESULTS"
     local BENCH="${OUTPUT_DIR//\//\\\/}"
-    local BENCH="${BENCH/results//}"
+    local BENCH="${BENCH/results/}"
     sed -e "s/OUTPUT_DIR/$BENCH/" -i "$CSV_RESULTS"
 
     # echo "$(csvsort -c EPSILON,FROM,TO "$CSV_RESULTS")" > "$CSV_RESULTS"
@@ -144,5 +89,6 @@ process_all_results() {
     local CSV_RESULTS="$2"
 
     cat "$OUTPUT_DIR"/results-*.csv > "$CSV_RESULTS"
+    # remove headers
     sed -e '1p;/,NODE.*/d' -i "$CSV_RESULTS"
 }
