1800e1636a73ad926bc9c2d473523a11c27c7dc3
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-03-18 16:04:24 +0100

    adapt compute script to graph_stats command line parsing

--- experiments/small_angles/compute.sh
+++ experiments/small_angles/compute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
 # requires bc to be installed for floating point operations
 
-EPSILON=("1.0" "0.5" "0.1")
+
+EPSILON=("1.0" "0.5" "0.2" "0.1")
 
 make_graph() {
 mkdir -p results/$1_$2
@@ -12,7 +12,7 @@ echo -e "3\n1\n0.0 0.0\n1.0 0.0\n0.0 $2\n0 1 2" > results/$1_$2/instance_$1_$2.g
 make_gl results/$1_$2/instance_$1_$2.graph results/$1_$2/instance_$1_$2.gl 1 1
 
 for e in "${EPSILON[@]}"; do
-	subdivision_stats graph results/$1_$2/instance_$1_$2.graph $e -h > results/$1_$2/stats_$e.csv
+	graph_stats --mode steiner_graph_size  -g results/$1_$2/instance_$1_$2.graph -e $e > results/$1_$2/stats_$e.csv
 done
 }
 
@@ -27,17 +27,16 @@ VAL=1.0
 for i in {0..48}; do
 	echo -n -e "\r$i, $VAL"
 	make_graph triangle $VAL
-	VAL=$(bc -l <<< "$VAL / 2")
+	VAL=$(bc -l <<< "$VAL / 2") # halve sin(alpha) here
 done
 echo -e "\r                              \rdone"
 
+# combine all files into one
 csvstack results/*/stats_*.csv > results/results.csv
 
 # split graph column into graph and sin
+sed -e 's/\.graph,/,/' -i results/results.csv
 sed -e 's/,\./,0\./' -i results/results.csv
-sed -e 's/graph,/graph,sin,/' -e 's/triangle_/triangle,/' -i results/results.csv
+sed -e 's/graph,/graph,sin,/' -e 's/instance_triangle_/triangle,/' -i results/results.csv
 echo "$(csvsort -c epsilon,sin -r results/results.csv)" > results/results.csv
 sed -e 's/True/1/' -i results/results.csv
-
-
-csvlook results/results.csv 
