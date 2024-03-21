21dce52cfded83d185a5e30aa6a614f52587bc72
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-03-21 13:45:26 +0100

    fix boxplotting script

--- experiments/shortest_paths/plot_over_epsilon.py
+++ experiments/shortest_paths/plot_over_epsilon.py
@@ -3,8 +3,11 @@
 # needs to be in PYTHONPATH
 import model.model as bench
 
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+
+import math
 
 import argparse
 import sys
@@ -20,7 +23,7 @@ def filter(data):
     data = data.loc[(data['cost'] != math.inf) & (data['cost'] != math.nan)]
 
     # check that exact value exists
-    has_reference = np.array([reference(data, row).shape[0] != 0 for i, row in data.iterrows()], dtype='bool')
+    has_reference = np.array([bench.reference(data, row).shape[0] != 0 for i, row in data.iterrows()], dtype='bool')
     if len(has_reference[(has_reference == False)]) != 0:
         print('exact value missing for some queries', file=sys.stderr)
         print(data.loc[has_reference == False], file=sys.stderr)
@@ -28,10 +31,10 @@ def filter(data):
 
     # check that coordinates of source and target coordinates match
     coords_match = np.array([
-        (reference(data, row)['source latitude'] == row['source latitude'])
-        & (reference(data, row)['source longitude'] == row['source longitude'])
-        & (reference(data, row)['target latitude'] == row['target latitude'])
-        & (reference(data, row)['target longitude'] == row['target longitude']).all() for i,row in data.iterrows() ], dtype='bool')
+        (bench.reference(data, row)['source latitude'] == row['source latitude'])
+        & (bench.reference(data, row)['source longitude'] == row['source longitude'])
+        & (bench.reference(data, row)['target latitude'] == row['target latitude'])
+        & (bench.reference(data, row)['target longitude'] == row['target longitude']).all() for i,row in data.iterrows() ], dtype='bool')
     if len(coords_match[(coords_match == False)]) != 0:
         print('mismatch in source/target coordinates', file=sys.stderr)
         print(data.loc[coords_match == False], file=sys.stderr)
@@ -51,13 +54,17 @@ def main():
     # load data and filter out unusable results
     data = bench.load(args.file)
     data = filter(data)
+    data.sort_values(by='epsilon', inplace=True)
 
     # by epsilon
-    columns_by_epsilon = [ data[data['epsilon'] == eps][args.column] for eps in data['epsilon'].unique() ]
+    column_by_epsilon = [ data[data['epsilon'] == eps][args.column] for eps in data['epsilon'].unique() ]
+
+    print('number of exact solutions: ')
+    print(len(data[data['epsilon'] == math.inf]))
 
     # generate box plots
     fig, ax = plt.subplots()
-    ax.boxplot(columns_by_epsilon, labels=data['epsilon'].unique())
+    ax.boxplot(column_by_epsilon, labels=[eps for eps in data['epsilon'].unique()])
     ax.set_xlabel('$\\varepsilon$')
     ax.set_ylabel(args.column)
 
