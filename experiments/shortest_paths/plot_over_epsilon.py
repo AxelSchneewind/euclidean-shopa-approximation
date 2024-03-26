c17d4095c348f643dff77d65011df63bfa8c8665
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-03-26 16:34:59 +0100

    add parameters to control boxplot details

--- experiments/shortest_paths/plot_over_epsilon.py
+++ experiments/shortest_paths/plot_over_epsilon.py
@@ -6,24 +6,15 @@ import model.model as bench
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-
 import math
-
 import argparse
 import sys
 
 
 
 def filter(data):
-    # check that cost value is finite
-    invalid_cost = data.loc[(data['cost'] == math.inf) | (data['cost'] == math.nan)]
-    if len(invalid_cost) != 0:
-        print('ignored by cost value: ', file=sys.stderr)
-        print(invalid_cost, file=sys.stderr)
-    data = data.loc[(data['cost'] != math.inf) & (data['cost'] != math.nan)]
-
     # check that exact value exists
-    has_reference = np.array([bench.reference(data, row).shape[0] != 0 for i, row in data.iterrows()], dtype='bool')
+    has_reference = np.array([bench.reference(data, row).shape[0] != 0 and bench.reference(data,row).iloc[0]['cost'] != math.inf for i, row in data.iterrows()], dtype='bool')
     if len(has_reference[(has_reference == False)]) != 0:
         print('exact value missing for some queries', file=sys.stderr)
         print(data.loc[has_reference == False], file=sys.stderr)
@@ -40,6 +31,24 @@ def filter(data):
         print(data.loc[coords_match == False], file=sys.stderr)
     data = data.loc[coords_match]
 
+    # check that cost value is finite
+    invalid_cost = data.loc[(data['cost'] == -math.inf) | (data['cost'] == math.inf) | (data['cost'] == math.nan)]
+    if len(invalid_cost) != 0:
+        print('ignored by cost value: ', file=sys.stderr)
+        print(invalid_cost, file=sys.stderr)
+    data = data.loc[(data['cost'] != math.inf) & (data['cost'] != -math.inf) & (data['cost'] != math.nan)]
+
+    # filter out results with invalid ratio
+    # print(data.dtypes)
+    # invalid_ratio = (data['ratio'] > 1.1)
+    # print(invalid_ratio)
+    # invalid_ratio = data.loc[invalid_ratio]
+    # if len(invalid_ratio) != 0:
+    #     print('ignored by ratio value: ', file=sys.stderr)
+    #     print(invalid_ratio, file=sys.stderr)
+    # data = data.loc[(data['ratio'] <= 1.5)]
+
+    print(len(data), 'usable values')
     return data
 
 
@@ -49,22 +58,32 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', '-f', required=True, action='append', help='path to the input file')
     parser.add_argument('--column', '-c', default='time', help='the column to show box plots of')
+    parser.add_argument('--fliers', action='store_true', help='whether to show fliers on boxplots')
+    parser.add_argument('--means', action='store_true', help='whether to show means on boxplots')
     args = parser.parse_args()
 
     # load data and filter out unusable results
     data = bench.load(args.file)
+
+    # 
+    data = data.loc[(data['benchmark'].str.contains('-triangle') == False)]
+    print(data['benchmark'].unique())
+
     data = filter(data)
+
+    data.sort_values(by='epsilon', inplace=True)
+    data.sort_index(inplace=True, ignore_index=True)
     data.sort_values(by='epsilon', inplace=True)
 
-    # by epsilon
-    column_by_epsilon = [ data[data['epsilon'] == eps][args.column] for eps in data['epsilon'].unique() ]
+    print('usable data contains ', len(np.unique(data[['source','target']], axis=0)), ' entries')
+    print(data)
 
-    print('number of exact solutions: ')
-    print(len(data[data['epsilon'] == math.inf]))
+    # by epsilon
+    column_by_epsilon = [ data.loc[data['epsilon'] == eps, args.column].to_numpy(dtype='float') for eps in data['epsilon'].unique() ]
 
     # generate box plots
     fig, ax = plt.subplots()
-    ax.boxplot(column_by_epsilon, labels=[eps for eps in data['epsilon'].unique()])
+    ax.boxplot(column_by_epsilon, labels=data['epsilon'].unique(), showfliers=args.fliers, showmeans=args.means)
     ax.set_xlabel('$\\varepsilon$')
     ax.set_ylabel(args.column)
 
