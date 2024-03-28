53ceb091bc3ccd2b141e2e41aa138c429bb4c17e
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-03-28 15:53:50 +0100

    add experiment for generating explicit graph from cutout

---
+++ experiments/explicit/compute.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+source ../utils.sh
+
+# number of queries
+NUM_QUERIES=100
+
+# maximum tree size to write to files (0 to disable tree output)
+TREE_SIZE=0
+
+# toggle A* here (on/off)
+ASTAR=on
+
+# graph files
+TRIANGULATION_REF_GRAPH=pruned-ref.graph
+TRIANGULATION_UNREF_GRAPH=pruned.graph
+EXPLICIT_REF_GRAPH=pruned-ref.fmi
+# EXPLICIT_GRAPH=pruned.fmi
+
+# output paths
+OUTPUT_DIR=results/aegaeis
+QUERY_FILE=results/aegaeis/queries.txt
+
+mkdir -p "$OUTPUT_DIR"
+
+# make queries
+if [ ! -f "$QUERY_FILE" ]; then
+	make_queries "$TRIANGULATION_UNREF_GRAPH" "$QUERY_FILE" "$NUM_QUERIES"
+fi
+
+
+############################ refined graph using triangle (Shewchuk) ############################
+# refined graph with steiner points
+if [ ! -d "$OUTPUT_DIR/approximate-ref" ]; then
+	EPSILONS=("1.0" "0.5" "0.25" "0.125" "0.0625" "0.03125" "0.015625")
+	for eps in "${EPSILONS[@]}"; do
+	    compute_single "$TRIANGULATION_REF_GRAPH" "$OUTPUT_DIR/approximate-ref" "$QUERY_FILE" "$eps" ""
+	done
+fi
+process_results "$OUTPUT_DIR/approximate-ref" "$OUTPUT_DIR/results-approximate-ref.csv" aegaeis-approximate-ref
+
+
+
+######################################## unrefined graph ########################################
+# unrefined graph with steiner points
+if [ ! -d "$OUTPUT_DIR/approximate-unref" ]; then
+	EPSILONS=("1.0" "0.5" "0.25")
+	for eps in "${EPSILONS[@]}"; do
+	    compute_single "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/approximate-unref" "$QUERY_FILE" "$eps" ""
+	done
+fi
+process_results "$OUTPUT_DIR/approximate-unref" "$OUTPUT_DIR/results-approximate-unref.csv" aegaeis-approximate-unref
+
+
+######################################### explicit graph ########################################
+# exact solutions
+if [ ! -d "$OUTPUT_DIR/explicit-ref" ]; then
+	compute_single "$EXPLICIT_GRAPH" "$OUTPUT_DIR/explicit-ref" "$QUERY_FILE" 0.0 ""
+fi
+process_results "$OUTPUT_DIR/explicit-ref" "$OUTPUT_DIR/results-explicit-ref.csv" aegaeis-explicit-ref
+
+
+
+######################################### postprocessing ########################################
+# aggregate results into one file
+process_all_results "$OUTPUT_DIR" "$OUTPUT_DIR"/results.csv
---
+++ experiments/explicit/generate_pruned.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+TRIANGLE_EXE=/opt/routing/graphs/aegaeis/triangle/triangle
+
+FILE_IN_UNREF=/opt/routing/graphs/aegaeis/aegaeis-unref.graph
+FILE_IN_UNREF_PROJ=aegaeis-unref-latlon.graph
+
+FILE_IN_REF=/opt/routing/graphs/aegaeis/aegaeis-ref-new.graph
+FILE_IN_REF_PROJ=aegaeis-ref-latlon.graph
+
+FILE_UNREF_GL=pruned.gl
+FILE_UNREF=pruned.graph
+
+FILE_REF_GL=pruned-ref.gl
+FILE_REF=pruned-ref.graph
+
+# 
+MINY=36.50861358642578125000
+MAXY=36.88759994506835937500
+MINX=24.09291076660156250000
+MAXX=24.71498107910156250000
+
+
+# project to latitude longitude
+project_graph $FILE_IN_UNREF $FILE_IN_UNREF_PROJ xy_to_latlon
+project_graph $FILE_IN_REF $FILE_IN_REF_PROJ xy_to_latlon
+
+# prune refined and unrefined graphs
+prune_graph -g $FILE_IN_UNREF_PROJ -o $FILE_UNREF -y $MINY -x $MINX -Y $MAXY -X $MAXX
+prune_graph -g $FILE_IN_REF_PROJ -o $FILE_REF -y $MINY -x $MINX -Y $MAXY -X $MAXX
+
+# make gl for pruned graphs
+make_gl $FILE_REF $FILE_REF_GL
+make_gl $FILE_UNREF $FILE_UNREF_GL
+
+# project back
+project_graph $FILE_UNREF $FILE_UNREF latlon_to_xy
+project_graph $FILE_REF $FILE_REF latlon_to_xy
+
+echo "pruned unrefined graph has size: "
+head -n2 $FILE_UNREF
+
+echo "pruned refined graph has size: "
+head -n2 $FILE_REF
