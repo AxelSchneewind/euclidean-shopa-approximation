651faf3d0ae918b7606148b3de6aa08de46a018d
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-03-28 16:41:08 +0100

    add usage hint

--- src/project.cpp
+++ src/project.cpp
@@ -4,11 +4,16 @@
 
 
 
-int main(int /*argc*/, const char* argv[]) {
-    std::cout << "Reading graph from " << argv[1] << "..." << std::flush;
+int main(int argc, const char* argv[]) {
+    if (argc < 3) {
+        std::cout << "usage " << argv[0] << " path/to/graph-file path/to/result [xy_to_latlon|latlon_to_xy]\n";
+        return 1;
+    }
+
+    std::cout << "Reading graph from " << argv[1] << "...";
     Graph graph;
     graph.read_graph_file(argv[1]);
-    std::cout << "\n, done" << std::endl;
+    std::cout << "\b\b\b, done" << std::endl;
 
     Projection projection = Projection::NONE;
     std::string projection_name(argv[3]);
@@ -17,12 +22,12 @@ int main(int /*argc*/, const char* argv[]) {
     if (projection_name == "latlon_to_xy")
         projection = Projection::WGS84_TO_GB;
 
-    std::cout << "projecting with " << (int)projection << "..." << std::flush;
+    std::cout << "projecting with " << (int)projection << "...";
     graph.project(projection);
 
     std::cout << "\b\b\b, done" << std::endl;
 
-    std::cout << "writing to " << argv[2] << "..." << std::flush;
+    std::cout << "writing to " << argv[2] << "...";
     graph.write_graph_file(argv[2]);
     std::cout << "\b\b\b, done" << std::endl;
 }
