bc4320668aab23288acc2dbb19cf689f73304160
Axel Schneewind <axel.schneewind@protonmail.com>
2024-03-29 17:05:59 +0100

    rename graphs and benchmarks

--- experiments/explicit/compute.sh
+++ experiments/explicit/compute.sh
@@ -19,8 +19,8 @@ EXPLICIT_REF_05_GRAPH=milos-ref-05.fmi
 # EXPLICIT_GRAPH=pruned.fmi
 
 # output paths
-OUTPUT_DIR=results/aegaeis
-QUERY_FILE=results/aegaeis/queries.txt
+OUTPUT_DIR=results/milos
+QUERY_FILE=results/milos/queries.txt
 
 mkdir -p "$OUTPUT_DIR"
 
@@ -32,25 +32,25 @@ fi
 
 ############################ refined graph using triangle (Shewchuk) ############################
 # refined graph with steiner points
-if [ ! -d "$OUTPUT_DIR/approximate-ref" ]; then
+if [ ! -d "$OUTPUT_DIR/implicit-ref" ]; then
 	EPSILONS=("1.0" "0.5" "0.25") # "0.125" "0.0625" "0.03125" "0.015625")
 	for eps in "${EPSILONS[@]}"; do
-	    compute_single "$TRIANGULATION_REF_GRAPH" "$OUTPUT_DIR/approximate-ref" "$QUERY_FILE" "$eps" ""
+	    compute_single "$TRIANGULATION_REF_GRAPH" "$OUTPUT_DIR/implicit-ref" "$QUERY_FILE" "$eps" ""
 	done
 fi
-process_results "$OUTPUT_DIR/approximate-ref" "$OUTPUT_DIR/results-approximate-ref.csv" aegaeis-approximate-ref
+process_results "$OUTPUT_DIR/implicit-ref" "$OUTPUT_DIR/results-implicit-ref.csv" milos-implicit-ref
 
 
 
 ######################################## unrefined graph ########################################
 # unrefined graph with steiner points
-if [ ! -d "$OUTPUT_DIR/approximate-unref" ]; then
+if [ ! -d "$OUTPUT_DIR/implicit-unref" ]; then
 	EPSILONS=("1.0" "0.5" "0.25")
 	for eps in "${EPSILONS[@]}"; do
-	    compute_single "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/approximate-unref" "$QUERY_FILE" "$eps" ""
+	    compute_single "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref" "$QUERY_FILE" "$eps" ""
 	done
 fi
-process_results "$OUTPUT_DIR/approximate-unref" "$OUTPUT_DIR/results-approximate-unref.csv" aegaeis-approximate-unref
+process_results "$OUTPUT_DIR/implicit-unref" "$OUTPUT_DIR/results-implicit-unref.csv" milos-implicit-unref
 
 
 ######################################### explicit graph ########################################
@@ -59,8 +59,8 @@ if [ ! -d "$OUTPUT_DIR/explicit-ref-05" ]; then
 	compute_single "$EXPLICIT_REF_GRAPH" "$OUTPUT_DIR/explicit-ref-10" "$QUERY_FILE" 1.0 ""
 	compute_single "$EXPLICIT_REF_05_GRAPH" "$OUTPUT_DIR/explicit-ref-05" "$QUERY_FILE" 0.5 ""
 fi
-process_results "$OUTPUT_DIR/explicit-ref-10" "$OUTPUT_DIR/results-explicit-ref-1.csv" aegaeis-explicit-ref
-process_results "$OUTPUT_DIR/explicit-ref-05" "$OUTPUT_DIR/results-explicit-ref-05.csv" aegaeis-explicit-ref
+process_results "$OUTPUT_DIR/explicit-ref-10" "$OUTPUT_DIR/results-explicit-ref-10.csv" milos-explicit-ref-10
+process_results "$OUTPUT_DIR/explicit-ref-05" "$OUTPUT_DIR/results-explicit-ref-05.csv" milos-explicit-ref-05
 
 
 
