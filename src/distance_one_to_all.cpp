cb94624cf07b4f06c1607d1baff60e70076d7417
Axel Schneewind <axel.schneewind@protonmail.com>
2024-03-29 14:29:15 +0100

    simplify one to all computation logic

--- src/distance_one_to_all.cpp
+++ src/distance_one_to_all.cpp
@@ -7,33 +7,24 @@
 
 int main(int argc, const char *argv[]) {
     if (argc < 5) {
-        std::cout << "usage ota path/to/graph path/to/output epsilon\n";
+        std::cout << "usage ota path/to/graph path/to/output epsilon source-id\n";
         return 1;
     }
 
     std::string graph_file(argv[1]);
     std::string output_file(argv[2]);
     std::ofstream output(output_file);
+    std::ofstream output_info(output_file + ".csv");
     double epsilon = std::stof(argv[3]);
+    long src = std::stol(argv[4]);
+
     Client client;
     client.read_graph_file(graph_file, epsilon);
-
     client.write_graph_stats(std::cout);
-
-    steiner_graph::triangle_node_id_type src = std::stoi(argv[4]);
-
-    output << "node,distance\n";
-
-    // init timing
-    auto before = std::chrono::high_resolution_clock::now();
 
     client.compute_one_to_all(src, output);
 
-    auto after = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double, std::milli> duration = after - before;
-
-    std::cout << "Time: " << duration.count() << "ms" << std::endl;
-
-    output.close();
+    client.write_query(std::cout);
+    client.write_csv_header(output_info);
+    client.write_csv(output_info);
 }
