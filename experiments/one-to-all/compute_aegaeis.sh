d2310327ee574cf2577924e3b83bf4308f590df8
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-05 14:06:55 +0200

    fix directory

--- experiments/one-to-all/compute_aegaeis.sh
+++ experiments/one-to-all/compute_aegaeis.sh
@@ -40,7 +40,7 @@ if [ ! -d "$OUTPUT_DIR/ref" ]; then
     compute_ota_queries "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/ref/$eps/" "$QUERY_FILE" "$eps"
   done
 fi
-process_results "$OUTPUT_DIR/ref" "$OUTPUT_DIR/results-ref.csv" aegaeis-ref
+process_results "$OUTPUT_DIR/ref/" "$OUTPUT_DIR/results-ref.csv" aegaeis-ref
 
 ############################ refined graph using triangle (Shewchuk) ############################
 # refined graph without points
@@ -53,7 +53,7 @@ if [ ! -d "$OUTPUT_DIR/triangle" ]; then
     compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle/$eps/" "$QUERY_FILE" "$eps" ""
   done
 fi
-process_results "$OUTPUT_DIR/triangle" "$OUTPUT_DIR/results-triangle.csv" aegaeis-triangle
+process_results "$OUTPUT_DIR/triangle/" "$OUTPUT_DIR/results-triangle.csv" aegaeis-triangle
 
 ######################################## unrefined graph ########################################
 # unrefined graph raw
@@ -66,14 +66,14 @@ if [ ! -d "$OUTPUT_DIR/unref" ]; then
     compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref/$eps/" "$QUERY_FILE" "$eps" ""
   done
 fi
-process_results "$OUTPUT_DIR/unref" "$OUTPUT_DIR/results-unref.csv" aegaeis-unref
+process_results "$OUTPUT_DIR/unref/" "$OUTPUT_DIR/results-unref.csv" aegaeis-unref
 
 ######################################## visibility graph #######################################
 # exact solutions
 if [ ! -d "$OUTPUT_DIR/vis" ]; then
   compute_ota_queries "$VISIBILITY_GRAPH" "$OUTPUT_DIR/vis/0.0/" "$QUERY_FILE" 0.0
 fi
-process_results "$OUTPUT_DIR/exact" "$OUTPUT_DIR/results-exact.csv" aegaeis-exact
+process_results "$OUTPUT_DIR/vis/" "$OUTPUT_DIR/results-exact.csv" aegaeis-exact
 
 ######################################### postprocessing ########################################
 # aggregate results into one file
