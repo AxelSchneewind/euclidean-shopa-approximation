ec6862eb48bd59a16c26ea9438f1354ae895eca4
Axel Schneewind <axel.schneewind@protonmail.com>
2024-04-07 15:19:08 +0200

    add plotting and summarizing to one-to-all experiment

---
+++ experiments/one-to-all/process.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PYTHONPATH=/opt/routing/lib
+export PYTHONPATH
+
+PLOT_FILE_TYPE=.pgf
+
+# triangle graph
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t triangle -c time --fliers -o boxplots_time_triangle$PLOT_FILE_TYPE
+python summarize.py -f results/aegaeis/results-triangle.csv --output-epsilon summaries/triangle-epsilon.csv --column 'time,memory usage final,queue pull count,queue push count,neighbors base node neighbors count,neighbors boundary node neighbors count,neighbors steiner point neighbors count'
+python summarize.py -f results/aegaeis/results-triangle.csv --output-queries summaries/triangle-queries.csv --column 'time,memory usage final,queue pull count,queue push count,neighbors base node neighbors count,neighbors boundary node neighbors count,neighbors steiner point neighbors count'
+
+# ref graph
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t ref -c time --fliers -o boxplots_time_ref$PLOT_FILE_TYPE
+python summarize.py -f results/aegaeis/results-ref.csv --output-epsilon summaries/ref-epsilon.csv --column 'time,memory usage final,queue pull count,queue push count,neighbors base node neighbors count,neighbors boundary node neighbors count,neighbors steiner point neighbors count'
+python summarize.py -f results/aegaeis/results-ref.csv --output-queries summaries/ref-queries.csv --column 'time,memory usage final,queue pull count,queue push count,neighbors base node neighbors count,neighbors boundary node neighbors count,neighbors steiner point neighbors count'
+
+# unref graph
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t unref -c time --fliers -o boxplots_time_ref$PLOT_FILE_TYPE
+python summarize.py -f results/aegaeis/results-unref.csv --output-epsilon summaries/unref-epsilon.csv --column 'time,memory usage final,queue pull count,queue push count,neighbors base node neighbors count,neighbors boundary node neighbors count,neighbors steiner point neighbors count'
+python summarize.py -f results/aegaeis/results-unref.csv --output-queries summaries/unref-queries.csv --column 'time,memory usage final,queue pull count,queue push count,neighbors base node neighbors count,neighbors boundary node neighbors count,neighbors steiner point neighbors count'
+
