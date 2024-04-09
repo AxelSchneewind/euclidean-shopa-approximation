16db4b609b3532c367a6f70f43ccedd12f7d0b48
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-09 18:15:37 +0200

    use = signs for cli parameter passing

--- experiments/shortest_paths/process.sh
+++ experiments/shortest_paths/process.sh
@@ -15,27 +15,27 @@ mkdir plots
 
 
 # triangle graph
-python plot_over_epsilon.py -f results/aegaeis/results.csv -t triangle -c ratio --fliers -o plots/boxplots_ratio_triangle$PLOT_FILE_TYPE
-python plot_over_epsilon.py -f results/aegaeis/results.csv -t triangle -c time --fliers -o plots/boxplots_time_triangle$PLOT_FILE_TYPE
-python summarize.py -f results/aegaeis/results-triangle.csv --output-epsilon summaries/triangle-epsilon.csv --column ratio
-python summarize.py -f results/aegaeis/results-triangle.csv --output-epsilon summaries/triangle-epsilon.csv --column time
-python summarize.py -f results/aegaeis/results-triangle.csv --output-queries summaries/triangle-queries.csv --column ratio
-python summarize.py -f results/aegaeis/results-triangle.csv --output-queries summaries/triangle-queries.csv --column time
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t=triangle -c ratio --fliers -o plots/boxplots_ratio_triangle$PLOT_FILE_TYPE
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t=triangle -c time --fliers -o plots/boxplots_time_triangle$PLOT_FILE_TYPE
+python summarize.py -f results/aegaeis/results-triangle.csv --output-epsilon=summaries/triangle-epsilon.csv --column=ratio
+python summarize.py -f results/aegaeis/results-triangle.csv --output-epsilon=summaries/triangle-epsilon.csv --column=time
+python summarize.py -f results/aegaeis/results-triangle.csv --output-queries=summaries/triangle-queries.csv --column=ratio
+python summarize.py -f results/aegaeis/results-triangle.csv --output-queries=summaries/triangle-queries.csv --column=time
 
 # ref graph
-python plot_over_epsilon.py -f results/aegaeis/results.csv -t ref -c ratio --fliers -o boxplots_ratio_ref$PLOT_FILE_TYPE
-python plot_over_epsilon.py -f results/aegaeis/results.csv -t ref -c time --fliers -o plots/boxplots_time_ref$PLOT_FILE_TYPE
-python summarize.py -f results/aegaeis/results-ref.csv --output-epsilon summaries/ref-epsilon.csv --column ratio
-python summarize.py -f results/aegaeis/results-ref.csv --output-epsilon summaries/ref-epsilon.csv --column time
-python summarize.py -f results/aegaeis/results-ref.csv --output-queries summaries/ref-queries.csv --column ratio
-python summarize.py -f results/aegaeis/results-ref.csv --output-queries summaries/ref-queries.csv --column time
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t=ref -c ratio --fliers -o boxplots_ratio_ref$PLOT_FILE_TYPE
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t=ref -c time --fliers -o plots/boxplots_time_ref$PLOT_FILE_TYPE
+python summarize.py -f results/aegaeis/results-ref.csv --output-epsilon=summaries/ref-epsilon.csv --column=ratio
+python summarize.py -f results/aegaeis/results-ref.csv --output-epsilon=summaries/ref-epsilon.csv --column=time
+python summarize.py -f results/aegaeis/results-ref.csv --output-queries=summaries/ref-queries.csv --column=ratio
+python summarize.py -f results/aegaeis/results-ref.csv --output-queries=summaries/ref-queries.csv --column=time
 
 # unref graph
-python plot_over_epsilon.py -f results/aegaeis/results.csv -t unref -c ratio --fliers -o plots/boxplots_ratio_ref$PLOT_FILE_TYPE
-python plot_over_epsilon.py -f results/aegaeis/results.csv -t unref -c time --fliers -o plots/boxplots_time_ref$PLOT_FILE_TYPE
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t=unref -c=ratio --fliers -o plots/boxplots_ratio_ref$PLOT_FILE_TYPE
+python plot_over_epsilon.py -f results/aegaeis/results.csv -t=unref -c=time --fliers -o plots/boxplots_time_ref$PLOT_FILE_TYPE
 
-python summarize.py -f results/aegaeis/results-unref.csv --output-epsilon summaries/unref-epsilon.csv --column ratio
-python summarize.py -f results/aegaeis/results-unref.csv --output-epsilon summaries/unref-epsilon.csv --column time
-python summarize.py -f results/aegaeis/results-unref.csv --output-queries summaries/unref-queries.csv --column ratio
-python summarize.py -f results/aegaeis/results-unref.csv --output-queries summaries/unref-queries.csv --column time
+python summarize.py -f results/aegaeis/results-unref.csv --output-epsilon=summaries/unref-epsilon.csv --column=ratio
+python summarize.py -f results/aegaeis/results-unref.csv --output-epsilon=summaries/unref-epsilon.csv --column=time
+python summarize.py -f results/aegaeis/results-unref.csv --output-queries=summaries/unref-queries.csv --column=ratio
+python summarize.py -f results/aegaeis/results-unref.csv --output-queries=summaries/unref-queries.csv --column=time
 
