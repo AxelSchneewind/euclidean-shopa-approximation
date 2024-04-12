32002b7cf03f53a32f80448b0c9da4a083abb1d0
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-12 11:22:26 +0200

    disable unref graph and add eps = 0.25

--- experiments/explicit/compute.sh
+++ experiments/explicit/compute.sh
@@ -14,8 +14,9 @@ ASTAR=on
 # graph files
 TRIANGULATION_REF_GRAPH=milos-ref.graph
 TRIANGULATION_UNREF_GRAPH=milos.graph
-EXPLICIT_REF_GRAPH=milos-ref-10.fmi
+EXPLICIT_REF_10_GRAPH=milos-ref-10.fmi
 EXPLICIT_REF_05_GRAPH=milos-ref-05.fmi
+EXPLICIT_REF_025_GRAPH=milos-ref-025.fmi
 # EXPLICIT_GRAPH=pruned.fmi
 
 # output paths
@@ -44,23 +45,25 @@ process_results "$OUTPUT_DIR/implicit-ref" "$OUTPUT_DIR/results-implicit-ref.csv
 
 ######################################## unrefined graph ########################################
 # unrefined graph with steiner points
-if [ ! -d "$OUTPUT_DIR/implicit-unref" ]; then
-	EPSILONS=("1.0" "0.5" "0.25")
-	for eps in "${EPSILONS[@]}"; do
-	    compute_ota_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref/$eps" "$QUERY_FILE" "$eps" ""
-	done
-fi
-process_results "$OUTPUT_DIR/implicit-unref" "$OUTPUT_DIR/results-implicit-unref.csv" milos-implicit-unref
+# if [ ! -d "$OUTPUT_DIR/implicit-unref" ]; then
+# 	EPSILONS=("1.0" "0.5" "0.25")
+# 	for eps in "${EPSILONS[@]}"; do
+# 	    compute_ota_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref/$eps" "$QUERY_FILE" "$eps" ""
+# 	done
+# fi
+# process_results "$OUTPUT_DIR/implicit-unref" "$OUTPUT_DIR/results-implicit-unref.csv" milos-implicit-unref
 
 
 ######################################### explicit graph ########################################
 # exact solutions
 if [ ! -d "$OUTPUT_DIR/explicit-ref-05" ]; then
-	compute_ota_queries "$EXPLICIT_REF_GRAPH" "$OUTPUT_DIR/explicit-ref/1.0" "$QUERY_FILE" 1.0 ""
+	compute_ota_queries "$EXPLICIT_REF_10_GRAPH" "$OUTPUT_DIR/explicit-ref/1.0" "$QUERY_FILE" 1.0 ""
 	compute_ota_queries "$EXPLICIT_REF_05_GRAPH" "$OUTPUT_DIR/explicit-ref/0.5" "$QUERY_FILE" 0.5 ""
+	compute_ota_queries "$EXPLICIT_REF_025_GRAPH" "$OUTPUT_DIR/explicit-ref/0.25" "$QUERY_FILE" 0.25 ""
 fi
 process_results "$OUTPUT_DIR/explicit-ref/1.0" "$OUTPUT_DIR/results-explicit-ref-10.csv" milos-explicit-ref-10
 process_results "$OUTPUT_DIR/explicit-ref/0.5" "$OUTPUT_DIR/results-explicit-ref-05.csv" milos-explicit-ref-05
+process_results "$OUTPUT_DIR/explicit-ref/0.5" "$OUTPUT_DIR/results-explicit-ref-025.csv" milos-explicit-ref-025
 
 
 
