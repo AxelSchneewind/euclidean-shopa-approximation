2bc0f28386b3e7ca913270c865d4f86b0e8fc71c
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-12 11:28:45 +0200

    add cumulative distributions

--- experiments/angle_distribution/plot.py
+++ experiments/angle_distribution/plot.py
@@ -9,6 +9,9 @@ import sys
 def read(filename):
     return pd.read_csv(filename)
 
+# plot histogram given by count y per value x
+def hist(x, y):
+    plt.plot(x,y)
 
 def main():
     if (len(sys.argv) < 2):
@@ -16,10 +19,27 @@ def main():
         exit(-1)
     
     data = read(sys.argv[1])
-    plt.plot(data['angle'], data['count'])
+
+    # compute cumulative distribution
+    current_sum = 0
+    hist_cumulative = []
+    for count in data['count']:
+        current_sum = current_sum + count
+        hist_cumulative = hist_cumulative + [current_sum]
+    data['count cumulative'] = hist_cumulative
+
+    # plot histogram
+    hist(data['angle'], data['count'])
     plt.xlabel('angle')
     plt.ylabel('count')
     plt.title('distribution of inner angles')
+    plt.show()
+
+    # plot cumulative histogram 
+    hist(data['angle'], data['count cumulative'])
+    plt.xlabel('angle')
+    plt.ylabel('count')
+    plt.title('cumulative distribution of inner angles')
     plt.show()
 
 if __name__ == '__main__':
