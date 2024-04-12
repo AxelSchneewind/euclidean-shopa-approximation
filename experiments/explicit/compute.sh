ab9f5facd02b06ef1fe5b551a3f897f8db60cc09
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-12 11:25:30 +0200

    fix invalid checks

--- experiments/explicit/compute.sh
+++ experiments/explicit/compute.sh
@@ -56,14 +56,18 @@ process_results "$OUTPUT_DIR/implicit-ref" "$OUTPUT_DIR/results-implicit-ref.csv
 
 ######################################### explicit graph ########################################
 # exact solutions
-if [ ! -d "$OUTPUT_DIR/explicit-ref-05" ]; then
+if [ ! -d "$OUTPUT_DIR/explicit-ref/1.0" ]; then
 	compute_ota_queries "$EXPLICIT_REF_10_GRAPH" "$OUTPUT_DIR/explicit-ref/1.0" "$QUERY_FILE" 1.0 ""
+fi
+if [ ! -d "$OUTPUT_DIR/explicit-ref/0.5" ]; then
 	compute_ota_queries "$EXPLICIT_REF_05_GRAPH" "$OUTPUT_DIR/explicit-ref/0.5" "$QUERY_FILE" 0.5 ""
+fi
+if [ ! -d "$OUTPUT_DIR/explicit-ref/0.25" ]; then
 	compute_ota_queries "$EXPLICIT_REF_025_GRAPH" "$OUTPUT_DIR/explicit-ref/0.25" "$QUERY_FILE" 0.25 ""
 fi
 process_results "$OUTPUT_DIR/explicit-ref/1.0" "$OUTPUT_DIR/results-explicit-ref-10.csv" milos-explicit-ref-10
 process_results "$OUTPUT_DIR/explicit-ref/0.5" "$OUTPUT_DIR/results-explicit-ref-05.csv" milos-explicit-ref-05
-process_results "$OUTPUT_DIR/explicit-ref/0.5" "$OUTPUT_DIR/results-explicit-ref-025.csv" milos-explicit-ref-025
+process_results "$OUTPUT_DIR/explicit-ref/0.25" "$OUTPUT_DIR/results-explicit-ref-025.csv" milos-explicit-ref-025
 
 
 
