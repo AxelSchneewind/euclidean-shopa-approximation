10486f7906d82118bb95a6d56f7181ff02eefdef
Axel Schneewind <axel.schneewind@protonmail.com>
2024-04-13 19:45:10 +0200

    cleanup and documentation

--- src/triangulation/fast_map.h
+++ src/triangulation/fast_map.h
@@ -4,38 +4,41 @@
 #include <vector>
 
 
+/**
+ * an offset array based map for two-dimensional keys.
+ * @tparam AggregateId first key
+ * @tparam IntraAggregateId second key
+ * @tparam Info elements to store
+ */
 template <typename AggregateId, typename IntraAggregateId, typename Info>
 class fast_map {
 private:
     using aggregate_id_type = AggregateId;
     using intra_aggregate_id_type = IntraAggregateId;
-    using index = int;
+    using index_type = int;
     using entry_type = Info;
 
-    std::vector<index> _edge_index;
+    std::vector<index_type> _aggregate_index;
     std::vector<entry_type> _entries;
 
-    entry_type _default_value;
-
 public:
-    using edge_id_type = AggregateId;
-    using intra_edge_id_type = IntraAggregateId;
     using info_type = Info;
 
-    fast_map (std::size_t edge_count, Info default_value);
+    fast_map (size_t aggregate_count, Info default_value);
 
     bool contains(aggregate_id_type id) const;
 
     void append(aggregate_id_type id, intra_aggregate_id_type count);
+    void append(aggregate_id_type id, intra_aggregate_id_type count, entry_type values);
 
-    info_type& at(edge_id_type edge_id, intra_edge_id_type intra_edge_id);
+    info_type& at(aggregate_id_type edge_id, intra_aggregate_id_type intra_edge_id);
 
-    info_type const& at(edge_id_type edge_id, intra_edge_id_type intra_edge_id) const;
+    info_type const& at(aggregate_id_type edge_id, intra_aggregate_id_type intra_edge_id) const;
 
-    template<typename Pair> requires requires(Pair t) {t.edge; t.steiner_index; }
+    template<typename Pair> requires requires(Pair t) {t.first(); t.second(); }
     info_type& operator[](Pair&& index);
 
-    template<typename Pair> requires requires(Pair t) {t.edge; t.steiner_index; }
+    template<typename Pair> requires requires(Pair t) {t.first(); t.second(); }
     info_type const& operator[](Pair&& index) const;
 
     void reset();
--- src/triangulation/fast_map_impl.h
+++ src/triangulation/fast_map_impl.h
@@ -6,66 +6,75 @@
 #include <vector>
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
-std::size_t fast_map<AggregateId, IntraAggregateId, Info>::edge_count() const { return _edge_index.size(); }
+std::size_t fast_map<AggregateId, IntraAggregateId, Info>::edge_count() const { return _aggregate_index.size(); }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
 void fast_map<AggregateId, IntraAggregateId, Info>::reset() { clear(); }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
 Info const &
-fast_map<AggregateId, IntraAggregateId, Info>::at(edge_id_type edge_id, intra_edge_id_type intra_edge_id) const {
-    assert(contains(edge_id));
-    return _entries[_edge_index[edge_id] + intra_edge_id];
+fast_map<AggregateId, IntraAggregateId, Info>::at(aggregate_id_type aggregate_id, intra_aggregate_id_type intra_aggregate_id) const {
+    assert(contains(aggregate_id));
+    return _entries[_aggregate_index[aggregate_id] + intra_aggregate_id];
 }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
-template<typename Pair> requires requires (Pair t) { t.edge; t.steiner_index; }
+template<typename Pair> requires requires (Pair t) { t.first(); t.second(); }
 typename fast_map<AggregateId, IntraAggregateId, Info>::info_type& fast_map<AggregateId, IntraAggregateId, Info>::
 operator[](Pair&& index) {
-    assert(contains(index.edge));
-    return _entries[_edge_index[index.edge] + index.steiner_index];
+    assert(contains(index.first()));
+    return _entries[_aggregate_index[index.first()] + index.second()];
 }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
-template<typename Pair> requires requires (Pair t) { t.edge; t.steiner_index; }
+template<typename Pair> requires requires (Pair t) { t.first(); t.second(); }
 typename fast_map<AggregateId, IntraAggregateId, Info>::info_type const& fast_map<AggregateId, IntraAggregateId, Info>::
 operator[](Pair&& index) const {
-    assert(contains(index.edge));
-    return _entries[_edge_index[index.edge] + index.steiner_index];
+    assert(contains(index.first()));
+    return _entries[_aggregate_index[index.first()] + index.second()];
 }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
 Info &
-fast_map<AggregateId, IntraAggregateId, Info>::at(edge_id_type edge_id, intra_edge_id_type intra_edge_id) {
-    assert(contains(edge_id));
-    return _entries[_edge_index[edge_id] + intra_edge_id];
+fast_map<AggregateId, IntraAggregateId, Info>::at(aggregate_id_type aggregate_id, intra_aggregate_id_type intra_aggregate_id) {
+    assert(contains(aggregate_id));
+    return _entries[_aggregate_index[aggregate_id] + intra_aggregate_id];
 }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
 void fast_map<AggregateId, IntraAggregateId, Info>::append(aggregate_id_type id, intra_aggregate_id_type count) {
-    assert(id >= 0 && static_cast<size_t>(id) < _edge_index.size());
+    assert(id >= 0 && static_cast<size_t>(id) < _aggregate_index.size());
 
-    _edge_index[id] = _entries.size();
-    _entries.resize(_entries.size() + count, _default_value);
+    _aggregate_index[id] = _entries.size();
+    _entries.resize(_entries.size() + count);
+
+    assert(contains(id));
+}
+
+template<typename AggregateId, typename IntraAggregateId, typename Info>
+void fast_map<AggregateId, IntraAggregateId, Info>::append(aggregate_id_type id, intra_aggregate_id_type count, entry_type values) {
+    assert(id >= 0 && static_cast<size_t>(id) < _aggregate_index.size());
+
+    _aggregate_index[id] = _entries.size();
+    _entries.resize(_entries.size() + count, values);
 
     assert(contains(id));
 }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
 bool fast_map<AggregateId, IntraAggregateId, Info>::contains(aggregate_id_type id) const {
-    assert(id >= 0 && static_cast<size_t>(id) < _edge_index.size());
-    return -1 != _edge_index[id];
+    assert(id >= 0 && static_cast<size_t>(id) < _aggregate_index.size());
+    return -1 != _aggregate_index[id];
 }
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
-fast_map<AggregateId, IntraAggregateId, Info>::fast_map(std::size_t edge_count, Info default_value)
-        : _edge_index(edge_count, -1)
-        , _default_value{default_value}
+fast_map<AggregateId, IntraAggregateId, Info>::fast_map(std::size_t aggregate_count, Info default_value)
+        : _aggregate_index(aggregate_count, -1)
 {}
 
 template<typename AggregateId, typename IntraAggregateId, typename Info>
 void fast_map<AggregateId, IntraAggregateId, Info>::clear() {
-    std::fill(_edge_index.begin(), _edge_index.end(), -1);
+    std::fill(_aggregate_index.begin(), _aggregate_index.end(), -1);
     _entries.clear();
     _entries.shrink_to_fit();
 }
