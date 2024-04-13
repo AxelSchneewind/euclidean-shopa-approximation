af45bed1efdf71ba160abe6e1eb1c322dc3c7cf1
Axel Schneewind <axel.schneewind@protonmail.com>
2024-04-13 20:02:18 +0200

    print error when radius too low

--- src/triangulation/subdivision_info_impl.h
+++ src/triangulation/subdivision_info_impl.h
@@ -25,20 +25,23 @@ subdivision::make_subdivision_info(const adjacency_list<int> &triangulation,
     result.resize(triangulation.edge_count());
 
 #pragma omp parallel for
-    for (size_t i = 0; i < triangulation.edge_count(); i++) {
-        auto node1 = triangulation.source(i);
-        auto node2 = triangulation.destination(i);
+    for (size_t edge_id = 0; edge_id < triangulation.edge_count(); edge_id++) {
+        auto node1 = triangulation.source(edge_id);
+        auto node2 = triangulation.destination(edge_id);
 
         // get coordinates
         auto const& c1 = nodes[node1].coordinates;
         auto const& c2 = nodes[node2].coordinates;
+
+        // length |e| of the edge
+        double const length = distance(c2, c1);
 
         // get minimal angle for node1 and node2
         // treat angles > 90 degrees like 90 degrees
         long double angle1 = std::numbers::pi_v<long double> / 2; // between node1->node2 and node1->node3
         long double angle2 = std::numbers::pi_v<long double> / 2; // between node2->node1 and node2->node3
 
-        for (auto&& edge: polyhedron.edges(i)) {
+        for (auto&& edge: polyhedron.edges(edge_id)) {
             if (optional::is_none(edge)) continue;
 
             // make sure node3 is different from node1 and node2
@@ -62,9 +65,6 @@ subdivision::make_subdivision_info(const adjacency_list<int> &triangulation,
         angle1 = std::clamp(angle1, 0.0l, std::numbers::pi_v<long double> / 2);
         angle2 = std::clamp(angle2, 0.0l, std::numbers::pi_v<long double> / 2);
 
-        // length |e| of the edge
-        double const length = distance(c2, c1);
-
         // relative value where the mid-point (with max distance to other edges) lies between node1 and node2
         long double const mid_position = 1.0l / (1.0l + (std::sin(angle1) / std::sin(angle2)));
         {
@@ -74,8 +74,12 @@ subdivision::make_subdivision_info(const adjacency_list<int> &triangulation,
 
         // distance values have been computed already, convert to r(v) relative to this edges length
         long double const factor = epsilon / 5;
-        long double const r_first  = std::clamp(factor * (r_values[node1] / length), min_r_value, 1.0l);
-        long double const r_second = std::clamp(factor * (r_values[node2] / length), min_r_value, 1.0l);
+        long double r_first  = factor * (r_values[node1] / length);
+        long double r_second = factor * (r_values[node2] / length);
+        if (r_first <= min_r_value || r_second <= min_r_value)
+            std::cerr << "a radius on edge " << edge_id << " is below the limit\n";
+        r_first  = std::clamp(r_first,  min_r_value, 1.0l);
+        r_second = std::clamp(r_second, min_r_value, 1.0l);
 
         // the base for computing relative node positions
         long double min_base_first = std::pow(r_first, -1.0 / (max_steiner_count_per_edge / 2));
@@ -86,7 +90,7 @@ subdivision::make_subdivision_info(const adjacency_list<int> &triangulation,
         long double const base_second = std::clamp(1.0l + epsilon * std::sin(angle2), min_base_second, 10.0l);
         if (base_first == min_base_first || base_second == min_base_second) {
             edges_capped++;
-            std::cerr << "number of points on edge " << i << " is bounded by index datatype (" << typeid(steiner_index_type).name() << ")\n";
+            std::cerr << "number of points on edge " << edge_id << " is bounded by index datatype (" << typeid(steiner_index_type).name() << ")\n";
         }
 
 
@@ -174,15 +178,15 @@ subdivision::make_subdivision_info(const adjacency_list<int> &triangulation,
         if (!is_in_range(count, 2UL, max_steiner_count_per_edge)
             || !is_in_range(mid_position, 0.0, 1.0)
             || !is_in_range(mid_index, 0UL, count - 1)
-            || !is_in_range(r_first,  0.0, 1.1)
-            || !is_in_range(r_second, 0.0, 1.1)
+            || !is_in_range(r_first,  min_r_value, 1.1)
+            || !is_in_range(r_second, min_r_value, 1.1)
             || !is_in_range(base_first,  min_base_first, std::numeric_limits<float>::max())
             || !is_in_range(base_second, min_base_second, std::numeric_limits<float>::max())
             || !is_in_range(left_count,  0UL, max_steiner_count_per_edge / 2)
             || !is_in_range(right_count, 0UL, max_steiner_count_per_edge / 2))
             throw std::invalid_argument("some value does not fit");
 
-        auto& entry = result[i];
+        auto& entry = result[edge_id];
         entry.mid_position = mid_position;
         entry.mid_index = mid_index;
         entry.node_count = count;
