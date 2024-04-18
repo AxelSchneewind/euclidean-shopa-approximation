4f290fe35cbcb9678c0381bec5433b241aeacefa
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-18 17:08:32 +0200

    add different pruning methods

--- experiments/one-to-all/compute_aegaeis.sh
+++ experiments/one-to-all/compute_aegaeis.sh
@@ -28,44 +28,102 @@ if [ ! -f "$QUERY_FILE" ]; then
   make_queries "$UNREF_TRIANGULATION_GRAPH" "$QUERY_FILE" "$NUM_QUERIES"
 fi
 
-# ######################################### refined graph ########################################
-# refined graph without points
+########################################## refined graph #########################################
+## pruned
 if [ ! -d "$OUTPUT_DIR/ref" ]; then
-  compute_ota_queries "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/ref/inf/" "$QUERY_FILE" inf
+  # refined graph without points
+  compute_ota_queries "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/ref/inf/" "$QUERY_FILE" inf "--pruning=prune"
 
   # refined graph with steiner points
   EPSILONS=("1.0" "0.5" "0.25")
   for eps in "${EPSILONS[@]}"; do
-    compute_ota_queries "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/ref/$eps/" "$QUERY_FILE" "$eps"
+    compute_ota_queries "$TRIANGULATION_GRAPH" "$OUTPUT_DIR/ref/$eps/" "$QUERY_FILE" "$eps" "--pruning=prune"
   done
 fi
 process_results "$OUTPUT_DIR/ref/" "$OUTPUT_DIR/results-ref.csv" aegaeis-ref
 
 ############################ refined graph using triangle (Shewchuk) ############################
-# refined graph without points
-if [ ! -d "$OUTPUT_DIR/triangle" ]; then
-  compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle/inf/" "$QUERY_FILE" inf ""
+## with pruning
+if [ ! -d "$OUTPUT_DIR/triangle-pruned" ]; then
+  # refined graph without points
+  compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle-pruned/inf/" "$QUERY_FILE" inf "--pruning=prune"
 
   # refined graph with steiner points
   EPSILONS=("1.0" "0.5" "0.25" "0.125" "0.0625" "0.03125" "0.015625")
   for eps in "${EPSILONS[@]}"; do
-    compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle/$eps/" "$QUERY_FILE" "$eps" ""
+    compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle-pruned/$eps/" "$QUERY_FILE" "$eps" "--pruning=prune"
   done
 fi
-process_results "$OUTPUT_DIR/triangle/" "$OUTPUT_DIR/results-triangle.csv" aegaeis-triangle
+process_results "$OUTPUT_DIR/triangle-pruned/" "$OUTPUT_DIR/results-triangle-pruned.csv" aegaeis-triangle-pruned
+
+## without pruning
+if [ ! -d "$OUTPUT_DIR/triangle-unpruned" ]; then
+  # refined graph without points
+  compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle-unpruned/inf/" "$QUERY_FILE" inf "--pruning=none"
+
+  # refined graph with steiner points
+  EPSILONS=("1.0" "0.5" "0.25" "0.125" "0.0625" "0.03125" "0.015625")
+  for eps in "${EPSILONS[@]}"; do
+    compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle-unpruned/$eps/" "$QUERY_FILE" "$eps" "--pruning=none"
+  done
+fi
+process_results "$OUTPUT_DIR/triangle-unpruned/" "$OUTPUT_DIR/results-triangle-unpruned.csv" aegaeis-triangle-unpruned
+
+## with pruning based on minimal bending angles
+if [ ! -d "$OUTPUT_DIR/triangle-pruned-min-angle" ]; then
+  # refined graph without points
+  compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle-pruned-min-angle/inf/" "$QUERY_FILE" inf "--pruning=prune-min-angle"
+
+  # refined graph with steiner points
+  EPSILONS=("1.0" "0.5" "0.25" "0.125" "0.0625" "0.03125" "0.015625")
+  for eps in "${EPSILONS[@]}"; do
+    compute_ota_queries "$TRIANGLE_TRIANGULATION_GRAPH" "$OUTPUT_DIR/triangle-pruned-min-angle/$eps/" "$QUERY_FILE" "$eps" "--pruning=prune-min-angle"
+  done
+fi
+process_results "$OUTPUT_DIR/triangle-pruned-min-angle/" "$OUTPUT_DIR/results-triangle-pruned-min-angle.csv" aegaeis-triangle-pruned-min-angle
+
 
 ######################################## unrefined graph ########################################
-# unrefined graph raw
-if [ ! -d "$OUTPUT_DIR/unref" ]; then
-  compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref/inf/" "$QUERY_FILE" inf ""
+## pruned
+if [ ! -d "$OUTPUT_DIR/unref-pruned" ]; then
+  # unrefined graph raw
+  compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref-pruned/inf/" "$QUERY_FILE" inf "--pruning=prune"
 
   # unrefined graph with steiner points
   EPSILONS=("1.0" "0.5" "0.25")
   for eps in "${EPSILONS[@]}"; do
-    compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref/$eps/" "$QUERY_FILE" "$eps" ""
+    compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref-pruned/$eps/" "$QUERY_FILE" "$eps" "--pruning=prune"
   done
 fi
-process_results "$OUTPUT_DIR/unref/" "$OUTPUT_DIR/results-unref.csv" aegaeis-unref
+process_results "$OUTPUT_DIR/unref-pruned/" "$OUTPUT_DIR/results-unref-pruned.csv" aegaeis-unref-pruned
+
+## unpruned
+if [ ! -d "$OUTPUT_DIR/unref-unpruned" ]; then
+  # unrefined graph raw
+  compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref-unpruned/inf/" "$QUERY_FILE" inf "--pruning=none"
+
+  # unrefined graph with steiner points
+  EPSILONS=("1.0" "0.5" "0.25")
+  for eps in "${EPSILONS[@]}"; do
+    compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref-unpruned/$eps/" "$QUERY_FILE" "$eps" "--pruning=none"
+  done
+fi
+process_results "$OUTPUT_DIR/unref-unpruned/" "$OUTPUT_DIR/results-unref-unpruned.csv" aegaeis-unref-unpruned
+
+## pruned by minimal bending angles
+if [ ! -d "$OUTPUT_DIR/unref-prune-min-angle" ]; then
+  # unrefined graph raw
+  compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref-prune-min-angle/inf/" "$QUERY_FILE" inf "--pruning=prune-min-angle"
+
+  # unrefined graph with steiner points
+  EPSILONS=("1.0" "0.5" "0.25")
+  for eps in "${EPSILONS[@]}"; do
+    compute_ota_queries "$UNREF_TRIANGULATION_GRAPH" "$OUTPUT_DIR/unref-prune-min-angle/$eps/" "$QUERY_FILE" "$eps" "--pruning=prune-min-angle"
+  done
+fi
+process_results "$OUTPUT_DIR/unref-prune-min-angle/" "$OUTPUT_DIR/results-unref-prune-min-angle.csv" aegaeis-unref-prune-min-angle
+
+
 
 ######################################## visibility graph #######################################
 # exact solutions
