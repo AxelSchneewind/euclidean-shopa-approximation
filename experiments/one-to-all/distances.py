40234c8eec208161795f5144b9ad3d940ef57dc5
Axel Schneewind <axel.schneewind@protonmail.com>
2024-04-21 17:42:34 +0200

    update distance script

--- experiments/one-to-all/distances.py
+++ experiments/one-to-all/distances.py
@@ -6,8 +6,9 @@ import pandas as pd
 
 
 def read_distances(file):
-    data = pd.read_csv(file, dtypes={ 'distance': np.float64, 1:np.int64 })
-    data.sort_values(by='node', inplace=True)
+    data = pd.read_csv(file, dtype={ 'distance': np.float64, 'node':np.int64 })
+    data.sort_values(by='distance', ascending=True, inplace=True)
+    data.drop_duplicates(subset=['node'], keep='first', inplace=True, ignore_index=True)
     return data
 
 def add_ratio(data, reference):
@@ -15,15 +16,21 @@ def add_ratio(data, reference):
     data['ratio'] = data[distance_column] / reference[distance_column]
     return data
 
-def describe(file, reference_file):
+def describe(file, reference_file, output):
     data = read_distances(file)
     data_ref = read_distances(reference_file)
 
     add_ratio(data, data_ref)
 
-    print(data['ratio'].describe())
+    description = data[['ratio']].describe().T
+    description['file'] = file
+    description['reference'] = reference_file
+    print(description)
+    description.to_csv(output)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        describe(sys.argv[0], sys.argv[1])
+    if len(sys.argv) > 3:
+        describe(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print('usage: ', sys.argv[0], 'distances.csv reference_distances.csv quality.csv')
