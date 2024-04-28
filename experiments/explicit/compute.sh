bba05f8291e480d3b6ee7b209073096d0965bc13
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-28 17:38:53 +0200

    fix storage argument

--- experiments/explicit/compute.sh
+++ experiments/explicit/compute.sh
@@ -3,7 +3,7 @@
 source ../utils.sh
 
 # number of queries
-NUM_QUERIES=1
+NUM_QUERIES=10
 
 # maximum tree size to write to files (0 to disable tree output)
 TREE_SIZE=0
@@ -24,7 +24,7 @@ mkdir -p "$OUTPUT_DIR"
 
 # make queries
 if [ ! -f "$QUERY_FILE" ]; then
-	make_queries "$TRIANGULATION_REF_GRAPH" "$QUERY_FILE" "$NUM_QUERIES"
+	make_ota_queries "$TRIANGULATION_REF_GRAPH" "$QUERY_FILE" "$NUM_QUERIES"
 fi
 
 # 
@@ -37,8 +37,8 @@ compute_bench() {
 
 	# 
 	if [[ "${GRAPH_FILE: -4}" == ".fmi" ]]; then
-		local STORAGE="explicit"
-		local BENCHMARK_NAME="milos-$STORAGE"
+		local STORAGE_TXT="explicit"
+		local BENCHMARK_NAME="milos-$STORAGE_TXT"
 		local DIRECTORY_NAME="$OUTPUT_DIR/$BENCHMARK_NAME"
 		local ARGUMENTS=""
 
@@ -46,33 +46,34 @@ compute_bench() {
 			compute_ota_queries "$EXPLICIT_REF_10_GRAPH" "$DIRECTORY_NAME/1.0" "$QUERY_FILE" 1.0 "$ARGUMENTS"
 			process_results "$DIRECTORY_NAME" "$OUTPUT_DIR/$BENCHMARK_NAME.csv" "$BENCHMARK_NAME"
 		fi
-		if [ ! -d "$DIRECTORY_NAME/0.5" ]; then
-			compute_ota_queries "$EXPLICIT_REF_05_GRAPH" "$DIRECTORY_NAME/0.5" "$QUERY_FILE" 0.5 "$ARGUMENTS"
-			process_results "$DIRECTORY_NAME" "$OUTPUT_DIR/$BENCHMARK_NAME.csv" "$BENCHMARK_NAME"
-		fi
-		if [ ! -d "$DIRECTORY_NAME/0.25" ]; then
-			compute_ota_queries "$EXPLICIT_REF_025_GRAPH" "$DIRECTORY_NAME/0.25" "$QUERY_FILE" 0.25 "$ARGUMENTS"
-			process_results "$DIRECTORY_NAME" "$OUTPUT_DIR/$BENCHMARK_NAME.csv" "$BENCHMARK_NAME"
-		fi
+		#if [ ! -d "$DIRECTORY_NAME/0.5" ]; then
+		#	compute_ota_queries "$EXPLICIT_REF_05_GRAPH" "$DIRECTORY_NAME/0.5" "$QUERY_FILE" 0.5 "$ARGUMENTS"
+		#	process_results "$DIRECTORY_NAME" "$OUTPUT_DIR/$BENCHMARK_NAME.csv" "$BENCHMARK_NAME"
+		#fi
+		#if [ ! -d "$DIRECTORY_NAME/0.25" ]; then
+		#	compute_ota_queries "$EXPLICIT_REF_025_GRAPH" "$DIRECTORY_NAME/0.25" "$QUERY_FILE" 0.25 "$ARGUMENTS"
+		#	process_results "$DIRECTORY_NAME" "$OUTPUT_DIR/$BENCHMARK_NAME.csv" "$BENCHMARK_NAME"
+		#fi
 
 	else
-		local STORAGE="implicit"
-		local BENCHMARK_NAME="$STORAGE-$PRUNING-$NEIGHBOR_FINDING"
+		local STORAGE_TXT="implicit"
+		local BENCHMARK_NAME="milos-$STORAGE_TXT-$PRUNING-$NEIGHBOR_FINDING"
 		local DIRECTORY_NAME="$OUTPUT_DIR/$BENCHMARK_NAME"
 		local ARGUMENTS="--pruning=$PRUNING --neighbor-finding=$NEIGHBOR_FINDING"
 
 		# 
 		if [[ "${STORAGE}" == "semi-explicit" ]]; then
-			local STORAGE="semi-explicit"
-			local BENCHMARK_NAME="milos-$STORAGE-$PRUNING-$NEIGHBOR_FINDING"
+			local STORAGE_TXT="semi-explicit"
+			local BENCHMARK_NAME="milos-$STORAGE_TXT-$PRUNING-$NEIGHBOR_FINDING"
 			local DIRECTORY_NAME="$OUTPUT_DIR/$BENCHMARK_NAME"
 			local ARGUMENTS="--pruning=$PRUNING --neighbor-finding=$NEIGHBOR_FINDING --coords-explicit"
 		fi
 
-		EPSILONS=("1.0" "0.5" "0.25")
+		EPSILONS=("1.0")
+		#"0.5" "0.25")
 		for eps in "${EPSILONS[@]}"; do
 			if [ ! -d "$DIRECTORY_NAME/$eps" ]; then
-		    		compute_shopa_queries "$GRAPH_FILE" "$DIRECTORY_NAME/$eps" "$QUERIES" "$eps" "$ARGUMENTS"
+		    		compute_ota_queries "$GRAPH_FILE" "${DIRECTORY_NAME}/$eps" "$QUERIES" "$eps" "${ARGUMENTS}"
 			fi
 		done
 		process_results "$DIRECTORY_NAME" "$OUTPUT_DIR/$BENCHMARK_NAME.csv" $BENCHMARK_NAME
@@ -116,7 +117,7 @@ compute_bench "$TRIANGULATION_REF_GRAPH" "prune-min-angle" "binary" "$QUERY_FILE
 # if [ ! -d "$OUTPUT_DIR/implicit-unref-unpruned" ]; then
 # 	EPSILONS=("1.0" "0.5" "0.25")
 # 	for eps in "${EPSILONS[@]}"; do
-# 	    compute_shopa_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref-unpruned/$eps" "$QUERY_FILE" "$eps" "--pruning=none"
+# 	    compute_ota_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref-unpruned/$eps" "$QUERY_FILE" "$eps" "--pruning=none"
 # 	done
 # fi
 # process_results "$OUTPUT_DIR/implicit-unref-unpruned" "$OUTPUT_DIR/results-implicit-unref-unpruned.csv" milos-implicit-unref-unpruned
@@ -125,7 +126,7 @@ compute_bench "$TRIANGULATION_REF_GRAPH" "prune-min-angle" "binary" "$QUERY_FILE
 # if [ ! -d "$OUTPUT_DIR/implicit-unref-pruned" ]; then
 # 	EPSILONS=("1.0" "0.5" "0.25")
 # 	for eps in "${EPSILONS[@]}"; do
-# 	    compute_shopa_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref-pruned/$eps" "$QUERY_FILE" "$eps" "--pruning=prune"
+# 	    compute_ota_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref-pruned/$eps" "$QUERY_FILE" "$eps" "--pruning=prune"
 # 	done
 # fi
 # process_results "$OUTPUT_DIR/implicit-unref-pruned" "$OUTPUT_DIR/results-implicit-unref-pruned.csv" milos-implicit-unref-pruned
@@ -134,7 +135,7 @@ compute_bench "$TRIANGULATION_REF_GRAPH" "prune-min-angle" "binary" "$QUERY_FILE
 # if [ ! -d "$OUTPUT_DIR/implicit-unref-pruned-min-angle" ]; then
 # 	EPSILONS=("1.0" "0.5" "0.25")
 # 	for eps in "${EPSILONS[@]}"; do
-# 	    compute_shopa_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref-pruned-min-angle/$eps" "$QUERY_FILE" "$eps" "--pruning=prune-min-angle"
+# 	    compute_ota_queries "$TRIANGULATION_UNREF_GRAPH" "$OUTPUT_DIR/implicit-unref-pruned-min-angle/$eps" "$QUERY_FILE" "$eps" "--pruning=prune-min-angle"
 # 	done
 # fi
 # process_results "$OUTPUT_DIR/implicit-unref-pruned-min-angle" "$OUTPUT_DIR/results-implicit-unref-pruned-min-angle.csv" milos-implicit-unref-pruned-min-angle
